a2804e86a82123f5d00be7fe0ea92c1fe2809893
Arshad <79403827+iam92786@users.noreply.github.com>
2024-12-01 16:22:51 +0530

    Update q04.cpp

--- interv/q04.cpp
+++ interv/q04.cpp
@@ -1,39 +1,52 @@
 #include <stdio.h>
 
-
-
+//METHOD-01
+void display(int dat[])
+{
+	for(int i = 0; i < 10; i++)
+	{
+		printf("%d\t%d\n",i, dat[i]);
+	}
+}
 int main (void)
 {
-	int fre[10] = {0};
-	int k;
-	
-	int arr[] = {1, 2, 3, 2, 1, 3, 4, 5, 1, 1, 1,7};
-	
-	printf("arr size %d\n",sizeof(arr));
-
-	//Find the Freq of array
-	for(int i = 0; arr[i]; i++)
+	int arr[] = {1,2,3,2,1,3,4,5,1,1,1,7};
+	int dat[10] = {0};
+	//printf("%d\n",arr[1]);
+	int len = (sizeof(arr)/sizeof(int));
+	printf("%d\n",len);	
+	for(int i = 0; i < 10; i++)
 	{
-		k = arr[i];
-		fre[k] = fre[k] + 1;
-		printf("%d\n",fre[k]);
-
+		for(int j = 0; j < len; j++)
+		{
+			//printf("i = %d  %d",i, );
+			if(i == arr[j])
+			{
+				//printf("-> %d",arr[j]);
+				dat[i] = dat[i] + 1;
+			}
+		}
+		printf("\n");
 	}
-	
-
-	//sort 
-
-	//display
-	for(int j = 0; j < 10; j++)
-	{
-		//printf(" = %d\n",fre[j]);
-		printf("fre[%d] = %d\n",j, fre[j]);
-
-	}
-
+	display(dat);
+	//sort
 	return 0;
 }
 
+/* OUT
+
+0	0
+1	5
+2	2
+3	2
+4	1
+5	1
+6	0
+7	1
+8	0
+9	0
+
+*/
 
 
 /*
